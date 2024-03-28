9fa6acba2a5de8b25cfe761b5295ece3c6d9f926
mynorgc79 <mynorguarchaj99@gmail.com>
2024-03-27 20:32:05 -0600

    refactor: agregando url render a allowed origins

--- core/settings.py
+++ core/settings.py
@@ -121,16 +121,16 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    "https://payroll-front.onrender.com",
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    "https://payroll-front.onrender.com",
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:5173',
+#     'http://localhost:3000',
+#     'http://localhost:8000',
+#     'http://127.0.0.1:8000',
+#     'http://127.0.0.1:3000',
+# ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
